006d9b94240db1f3830bbc3a5bbdbd3f321c0bc8
Sanjay Govind <sanjay.govind9@gmail.com>
2022-03-18 20:32:45 +1300

    asyncify

--- example.py
+++ example.py
@@ -1,6 +1,11 @@
+import asyncio
 from solutions3000 import Panel, UserType, ArmType
-panel = Panel(7700, 'ip', UserType.InstallerApp, 'automationcode', 'alink code')
-panel.initialise()
-panel.update_status()
-panel.arm(ArmType.Stay, panel.areas)
-print(panel)
+
+async def client():
+    panel = Panel(7700, 'ip', UserType.InstallerApp, 'automationcode', 'alink code')
+    await panel.initialise()
+    await panel.update_status()
+    # panel.arm(ArmType.Stay, panel.areas)
+    print(panel)
+
+asyncio.run(client())
