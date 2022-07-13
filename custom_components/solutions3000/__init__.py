3ef0456bcad9c49a948266af3ed92b58b98efd0f
Sanjay Govind <sanjay.govind9@gmail.com>
2022-07-14 09:29:03 +1200

    replace async_setup_platforms with async_forward_entry_setups

--- custom_components/solutions3000/__init__.py
+++ custom_components/solutions3000/__init__.py
@@ -39,7 +39,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = {"panel_update": panel_update, "unsub_options": unsub_options_update_listener}
     await panel_update.async_config_entry_first_refresh()
 
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
